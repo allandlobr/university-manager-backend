e87338cecee58098c37b7e1a8bc2eaf52ac8dfe1
allandlobr <allandlobr@gmail.com>
2023-11-12 11:48:00 -0300

    New dependencies added and app module updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule, UsersModule, RoleModule],
   controllers: [AppController],
   providers: [AppService],
 })
